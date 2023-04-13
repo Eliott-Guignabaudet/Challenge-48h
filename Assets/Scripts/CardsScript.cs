05f65f8fa3b8d0e79d404551e0d382b4f0baf996
aidandumont2 <aidan.dumont@yahoo.com>
2023-04-13 19:56:18 +0200

    deck début

--- Assets/Scripts/CardsScript.cs
+++ Assets/Scripts/CardsScript.cs
@@ -6,15 +6,9 @@ public class CardsScript : MonoBehaviour
 {
 
     public Carte carteSO;
-    // Start is called before the first frame update
     void Start()
     {
         gameObject.GetComponent<SpriteRenderer>().sprite = carteSO.image;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
