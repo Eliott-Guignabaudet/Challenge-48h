330766140a7f7c99bb481e2dfb2222fd9e796443
aidandumont2 <aidan.dumont@yahoo.com>
2023-04-13 20:07:25 +0200

    réglages

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -8,15 +8,15 @@ public class Deck : MonoBehaviour
 
     public List<Carte> deck = new List<Carte>();
     public static int nbCartesInDeck = 20;
-    private int[] listIndexCartes = new int[nbCartesInDeck];
-    private bool isIndexValid = false;
+    /*private int[] listIndexCartes = new int[nbCartesInDeck];
+    private bool isIndexValid = false;*/
 
     void Start()
     {
 
-       for (int i = 0; i < nbCartesInDeck; i++){
+       /*for (int i = 0; i < nbCartesInDeck; i++){
             int index = Random.Range(0, 30);
-            /*for (int indexCartesInList = 0; indexCartesInList <= listIndexCartes.Length; indexCartesInList++)
+            for (int indexCartesInList = 0; indexCartesInList <= listIndexCartes.Length; indexCartesInList++)
             {
                if (index != listIndexCartes[indexCartesInList])
                 {
@@ -31,11 +31,11 @@ public class Deck : MonoBehaviour
             {
                 
                 isIndexValid = false;
-            }*/
+            }
             Debug.Log(index);
            listIndexCartes[i] = index;
         }
-        Debug.Log(listIndexCartes);
+        Debug.Log(listIndexCartes);*/
     }
 
     // Update is called once per frame
