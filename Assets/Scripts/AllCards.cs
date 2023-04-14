d469427f1d4752ddd1d2fb00af505196250cf697
aidandumont2 <aidan.dumont@yahoo.com>
2023-04-14 18:02:05 +0200

    add carte didacticiel

--- Assets/Scripts/AllCards.cs
+++ Assets/Scripts/AllCards.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class AllCards : MonoBehaviour
 {
-
+    public Carte didactitielCard;
     public List<Carte> deck = new List<Carte>();
     public static int nbCartesInDeck;
     /*private int[] listIndexCartes = new int[nbCartesInDeck];
@@ -16,7 +16,8 @@ public class AllCards : MonoBehaviour
     {
         System.Random random = new System.Random();
         var shuffled = deck.OrderBy(_ => random.Next()).ToList();
-        deck = shuffled.Take(20).ToList();
+        deck = shuffled.Take(21).ToList();
+        deck[0] = didactitielCard;
 
     }
 
