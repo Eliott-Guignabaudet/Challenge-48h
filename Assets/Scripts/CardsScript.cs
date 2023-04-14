9f98e73ebc2357ca796ebfd1469ca5e8c05bc203
aidandumont2 <aidan.dumont@yahoo.com>
2023-04-14 11:23:42 +0200

    playtest sans tirage aléatoire

--- Assets/Scripts/CardsScript.cs
+++ Assets/Scripts/CardsScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CardsScript : MonoBehaviour
 {
+    public Player player;
     private int indexListCards = 0;
     public bool isSwipLeft = false;
     public bool isSwipRight = false;
@@ -20,13 +21,29 @@ public class CardsScript : MonoBehaviour
         if (indexListCards == listeCartes.deck.Count)
         {
             Debug.Log("Fin du jeu");
+            player.Win();
         }
         else if (isSwipLeft)
         {
             //Appliquer les malus/bonus à l'UI barres etat
+            player.AddRemoveHunger(listeCartes.deck[indexListCards].statsGauche.influenceHunger);
+            player.AddRemoveKnowledge(listeCartes.deck[indexListCards].statsGauche.influenceKnowledge);
+            player.AddRemoveSocial(listeCartes.deck[indexListCards].statsGauche.influenceSocial);
+            player.AddRemoveSold(listeCartes.deck[indexListCards].statsGauche.influenceSold);
             indexListCards += 1;
             gameObject.GetComponent<SpriteRenderer>().sprite = listeCartes.deck[indexListCards].image;
             isSwipLeft = false;
+        }
+        else if (isSwipRight)
+        {
+            //Appliquer les malus/bonus à l'UI barres etat
+            player.AddRemoveHunger(listeCartes.deck[indexListCards].statsDroite.influenceHunger);
+            player.AddRemoveKnowledge(listeCartes.deck[indexListCards].statsDroite.influenceKnowledge);
+            player.AddRemoveSocial(listeCartes.deck[indexListCards].statsDroite.influenceSocial);
+            player.AddRemoveSold(listeCartes.deck[indexListCards].statsDroite.influenceSold);
+            indexListCards += 1;
+            gameObject.GetComponent<SpriteRenderer>().sprite = listeCartes.deck[indexListCards].image;
+            isSwipRight = false;
         }
     }
 
@@ -35,5 +52,9 @@ public class CardsScript : MonoBehaviour
         isSwipLeft = true;
     }
 
+    public void SimulSwipRight()
+    {
+        isSwipRight = true;
+    }
 
 }
